51098dec339ac48ab613210e8482a033e62a2e09
Naman-14 <naman_14@outlook.com>
2023-03-17 13:15:45 +0530

    adding files

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "NickEC2" {
     ami           = "ami-0d81306eddc614a45"
-    subnet        = "subnet-0962069167a59d185"
+    subnet_id     = "subnet-0962069167a59d185"
     instance_type = "t2.micro"
 
     tags = {
