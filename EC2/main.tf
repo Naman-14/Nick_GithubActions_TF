a145c2ea4ac7c5ccbf4a1d096cdeb87d86af294a
Naman-14 <naman_14@outlook.com>
2023-03-17 13:10:13 +0530

    adding files

---
+++ EC2/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "NickEC2" {
+    ami           = "ami-0d81306eddc614a45"
+    subnet        = "subnet-0962069167a59d185"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "Naman Moolri"
+        Owner = "naman.moolri@cloudeq.com"
+    }
+    volume_tags = {
+        "Name" = "Naman Moolri"
+        "Owner" = "naman.moolri@cloudeq.com"
+    }
+}
